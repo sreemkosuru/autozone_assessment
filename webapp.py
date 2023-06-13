95a1fb054399545ba57c6d81bba4070ce4ffd882
Sree Mukunda Kosuru <sree@Srees-MacBook-Air.local>
2023-06-13 13:00:16 -0400

    remove hello world comment

--- webapp.py
+++ webapp.py
@@ -1,13 +1,3 @@
-"""from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello there!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)"""
 
 from flask import Flask, request, jsonify
 
